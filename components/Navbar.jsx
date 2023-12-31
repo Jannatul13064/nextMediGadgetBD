92eb769f571e006095a564365a74431fa4e38c54
Akash Islam <akzzislam19194@gmail.com>
2024-01-01 01:43:41 +0600

    nav update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar sticky top-0 bg-gradient-to-r from-sky-200 to-indigo-400 bg-clip-padding backdrop-filter backdrop-blur-6xl bg-opacity-80">
+    <div className="navbar sticky top-0 bg-gradient-to-r from-sky-200 to-indigo-400 bg-clip-padding backdrop-filter backdrop-blur-6xl">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
